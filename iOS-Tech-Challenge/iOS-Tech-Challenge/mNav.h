508cfacbce4a095bd0e16fe39859de08b3da8770
AKI <nobodyyu@gmail.com>
2015-09-19 22:13:34 +0800

    Customized UINavigationController

---
+++ iOS-Tech-Challenge/iOS-Tech-Challenge/mNav.h
@@ -0,0 +1,13 @@
+//
+//  mNav.h
+//  iOS-Tech-Challenge
+//
+//  Created by AKI on 2015/9/19.
+//  Copyright © 2015年 AKI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface mNav : UINavigationController
+
+@end

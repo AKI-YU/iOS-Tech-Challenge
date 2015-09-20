b244939edc0cb5455fd9104d3c76bf3427d40d3c
cjlin <ejameslin@gmail.com>
2015-09-20 16:07:40 +0800

    進貨 連動 庫存

--- iOS-Tech-Challenge/iOS-Tech-Challenge/Constants.h
+++ iOS-Tech-Challenge/iOS-Tech-Challenge/Constants.h
@@ -11,5 +11,17 @@
 
 #define SideMenuTitles @[@"管理",@"進貨訂貨",@"庫存盤點",@"其他"]
 
+#define ParseClassWareHouse @"warehouse"
+#define ParseClassWareHouseProduct @"product_name"
+#define ParseClassWareHouseAmount @"amount"
+#define ParseClassWareHouseFresh @"p_fresh_time"
+#define ParseClassWareHousePurchase @"purchase"
+#define ParseClassWareHousePurchaseID @"purchase_id"
+
+#define ParseClassPurchase @"purchase"
+#define ParseClassPurchaseAmount @"amount"
+#define ParseClassPurchaseName @"name"
+#define ParseClassPurchaseArriveDate @"arrive_date"
+#define ParseClassPurchasePurchaseID @"purchase_id"
 
 #endif /* Constants_h */

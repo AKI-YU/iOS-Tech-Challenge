4b40bf97d0cbecf533a209f92278f6a1a8943e4a
Á´ãÊòáÈô≥ <tasb00428@yahoo.com.tw>
2015-09-20 09:14:49 +0800

    test commit

--- iOS-Tech-Challenge/iOS-Tech-Challenge/Parse/ParseAPI.m
+++ iOS-Tech-Challenge/iOS-Tech-Challenge/Parse/ParseAPI.m
@@ -6,19 +6,86 @@
 //  Copyright © 2015 AKI. All rights reserved.
 //
 
-#define Product @""
+
+
+//Parse Class Name
+#define AWS_PRODUCT @"AWS_Product"
+#define COMPANY_DETAIL @"companyDetail"
+#define AWS_ORDER @"AWS_Order"
+
+
+//Parse AWS_Order Name
+#define ORDER_ID @"order_id"
+#define NAME @"name"
+#define AMOUNT @"amount"
+#define ORDER_PPL @"order_ppl"
+#define PRICE @"price"
+
+
+
+#define NO_DATA_LOG NSLog(@"Did Not Get Data");
+
+
 
 #import "ParseAPI.h"
 
 @implementation ParseAPI
 
-
-+(PFQuery*)productMenu{
++(void)aws_orderWithOrderID:(NSInteger)order_id     //訂單編號
+                productName:(NSString*)productName  //訂單產品名稱
+                orderAmount:(NSInteger)amount       //訂單數量
+                   orderPPL:(NSString*)order_ppl    //訂單人
+                      price:(NSInteger)pirce        //訂單價錢
+                andCallback:(void (^)(BOOL))callback{
     
-    PFQuery *query = [PFQuery queryWithClassName:@"AWS_Product"];
     
-    return query;
+    PFObject *newGroup = [PFObject objectWithClassName:AWS_ORDER];
+    newGroup[ORDER_ID] = [NSNumber numberWithInteger:order_id];
+    newGroup[NAME] = productName;
+    newGroup[AMOUNT] = [NSNumber numberWithInteger:amount];
+    newGroup[ORDER_PPL] = order_ppl;
+    newGroup[PRICE] = [NSNumber numberWithInteger:pirce];
+    [newGroup saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error)
+     {
+         return callback(succeeded);
+     }];
+    
     
 }
++(void)aws_productMenu:(void (^)(NSArray *))callback{
+    
+    PFQuery *query = [PFQuery queryWithClassName:AWS_PRODUCT];
+    
+    [query findObjectsInBackgroundWithBlock:^(NSArray *results, NSError *error) {
+        
+        if (results)
+            callback(results);
+        else
+            NO_DATA_LOG
+            }];
+}
+
++(void)aws_companyDetail:(void (^)(NSArray *))callback{
+    
+    
+    PFQuery *query = [PFQuery queryWithClassName:COMPANY_DETAIL];
+    
+    [query findObjectsInBackgroundWithBlock:^(NSArray *results, NSError *error) {
+        
+        if (results) {
+            if (results)
+                callback(results);
+            else
+                NO_DATA_LOG
+                }
+    }];
+    
+    //            NSLog(@"results");
+    //            NSMutableArray *arr = [[NSMutableArray alloc] initWithCapacity:results.count];
+    //            for (PFObject *obj in results) {
+    //                [arr addObject:obj[@"company"]];
+    //            }
+}
+
 
 @end
